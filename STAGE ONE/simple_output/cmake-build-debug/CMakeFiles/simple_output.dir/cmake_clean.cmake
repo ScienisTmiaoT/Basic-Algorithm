71dba51bd2e228ee4173c2a60d5bd6a091929f0e
ScienisTmiaoT <1730402302@qq.com>
2016-12-22 20:40:07 +0800

    calculate the time to execute program

---
+++ STAGE ONE/simple_output/cmake-build-debug/CMakeFiles/simple_output.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_output.dir/main.c.obj"
+  "simple_output.pdb"
+  "simple_output.exe"
+  "simple_output.exe.manifest"
+  "libsimple_output.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simple_output.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ STAGE ONE/simple_output/main.c
@@ -0,0 +1,23 @@
+#include <math.h>
+#include <time.h>
+#include "stdio.h"
+
+int main ()
+{
+    const int MOD = 1000000;
+    int n, s = 0;
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++)
+    {
+        int factorial = 1;
+        for(int j = 1; j <= i; j++)
+        {
+            factorial = (factorial * j % MOD);
+        }
+        s = (s + factorial) % MOD;
+    }
+    printf("%d\n", s);
+    // calculate the time to process
+    printf("Time used = %.2f\n", (double)clock() / CLOCKS_PER_SEC);
+    return 0;
+}
